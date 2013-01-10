744d6843dd5c243744dd983de05e4b287622bebc
jstaffans <johannes.staffans@gmail.com>
2013-01-10 10:56:43 +0200

    fontconfig dependency, fixed path issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,9 @@
 class phantomjs($version = "1.8.1" ) {
 
+    $packages = ["fontconfig"]
+
+    package { $packages: ensure => installed}
+
     if $::architecture == "amd64" or $::architecture == "x86_64" {
         $platid = "x86_64"
     } else {
@@ -7,21 +11,22 @@ class phantomjs($version = "1.8.1" ) {
     }
 
     $filename = "phantomjs-${version}-linux-${platid}.tar.bz2"
+    $filename_unpacked = "phantomjs-${version}-linux-${platid}.tar"
     $phantom_src_path = "/usr/local/src/phantomjs-${version}/"
-    $phantom_bin_path = "/opt/phantomjs/"
+    $phantom_bin_path = "/opt/phantomjs-${version}-linux-${platid}/"
 
     file { $phantom_src_path : ensure => directory }
 
     exec { "download-${filename}" : 
-        command => "wget http://phantomjs.googlecode.com/files/${filename} -O ${filename}",
+        command => "/usr/bin/wget http://phantomjs.googlecode.com/files/${filename} -O ${filename}",
         cwd => $phantom_src_path,
         creates => "${phantom_src_path}${filename}",
         require => File[$phantom_src_path]
     }
     
     exec { "extract-${filename}" :
-        command     => "tar xvfz ${filename} -C /opt/",
-        creates     => "/opt/phantomjs/",
+        command     => "/bin/bunzip2 ${filename_unpacked}; /bin/tar xvf ${filename} -C /opt/",
+        creates     => "/opt/phantomjs-${version}-linux-${platid}/",
         cwd         => $phantom_src_path,
         require     => Exec["download-${filename}"],
     }
@@ -39,8 +44,8 @@ class phantomjs($version = "1.8.1" ) {
     }
     
     exec { "nuke-old-version-on-upgrade" :
-        command => "rm -Rf /opt/phantomjs /usr/local/bin/phantomjs",
-        unless => "test -f /usr/local/bin/phantomjs && /usr/local/bin/phantomjs --version | grep ${version}",
+        command => "/bin/rm -Rf /opt/phantomjs /usr/local/bin/phantomjs",
+        unless => "/usr/bin/test -f /usr/local/bin/phantomjs && /usr/local/bin/phantomjs --version | grep ${version}",
         before => Exec["download-${filename}"]
     }
 
